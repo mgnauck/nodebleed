83f99079fd5d885af3531aac41a9fcd7bab74db8
Markus Gnauck <17846097+mgnauck@users.noreply.github.com>
2025-07-19 23:48:08 +0200

    TODO

--- main.c
+++ main.c
@@ -299,6 +299,7 @@ int main(void)
 	// TODO Import from blender custom bin export
 	// TODO Subdiv surfaces
 	// TODO Move code from main into some subsys
+	// TODO Build step that outputs compressed version
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		exit(1);
